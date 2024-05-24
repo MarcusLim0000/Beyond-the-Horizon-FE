0b44e6ca07cfcec25474ff072da41d9913748ea0
Marcus Lim <marcus.lwc7@gmail.com>
2024-05-24 20:39:08 +0800

    added routing and homepage

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+
+    return (
+      <>
+        <div>
+            <p className='introduction'>
+              Beyond the Horizon is a travel app that will help you to plan and work on future holidays! Happy Holidays!
+            </p>
+        </div>
+      </>
+    )
+  }
+
+export default Home;
---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,14 @@
+function SignUp() {
+
+    return (
+      <>
+        <div>
+            <p>
+              This is the sign up page
+            </p>
+        </div>
+      </>
+    )
+  }
+
+export default SignUp;
