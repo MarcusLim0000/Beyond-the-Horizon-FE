2f587fdbeb591a9b224ab312f00cfec4603c2006
Marcus_lim <marcus.lwc7@gmail.com>
2024-05-26 15:16:35 +0800

    added forms for signin and up

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,14 +1,55 @@
-function SignIn() {
+import "./SignIn.css";
+import React, { useState } from "react";
+// import * as usersService from "utilities-users-service"
 
-    return (
-      <>
-        <div>
-            <p>
-              This is the sign in page
-            </p>
-        </div>
-      </>
-    )
+function SignIn({ setUser }) {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+
+  function handleChange(evt) {
+    setState({ ...state, [evt.target.name]: evt.target.value, error: "" });
   }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const user = await usersService.login(state);
+      setUser(user);
+    } catch {
+      setError("Log in failed - try again");
+    }
+  }
+
+  return (
+    <>
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <label>Email</label>
+          <input
+            name="email"
+            placeholder="Email"
+            value={state.email}
+            type="email"
+            onChange={handleChange}
+          />
+          <label>Password</label>
+          <input
+            name="password"
+            placeholder="Password"
+            value={state.password}
+            type="password"
+            onChange={handleChange}
+          />
+          <button type="submit">LOG IN</button>
+        </form>
+      </div>
+    </>
+  );
+}
 
 export default SignIn;
