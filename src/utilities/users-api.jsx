89e31475961a5281938dc649504d591a513a544d
Marcus Lim <marcus.lwc7@gmail.com>
2024-06-11 23:16:58 +0800

    reorganized components to start with create holiday then forms inside

--- src/utilities/users-api.jsx
+++ src/utilities/users-api.jsx
@@ -1,5 +1,4 @@
 import sendRequest from "./send-request";
-import axios from "axios";
 
 const BASE_URL = 'http://localhost:3000'
 
@@ -11,12 +10,12 @@ export function login(userInput) {
     return sendRequest(`${BASE_URL}/api/users/sign-in`, 'POST', userInput)
 }
 
-export function deleteListing(id) {
-    return sendRequest(`${BASE_URL}/api/flight/delete/${id}`,'DELETE')
-}
-
 export function createFlight(userInput) {
     return sendRequest(`${BASE_URL}/api/flight/create`, 'POST', userInput)
+}
+
+export function createHoliday(userInput) {
+    return sendRequest(`${BASE_URL}/api/holiday/create`, 'POST', userInput)
 }
 
 export function createHotel(userInput) {
@@ -27,18 +26,18 @@ export function createEvent(userInput) {
     return sendRequest(`${BASE_URL}/api/event/create`, 'POST', userInput)
 }
 
+export function deleteHoliday(id) {
+    return sendRequest(`${BASE_URL}/api/holiday/delete/${id}`,'DELETE')
+}
+
+export function deleteFlight(id) {
+    return sendRequest(`${BASE_URL}/api/flight/delete/${id}`,'DELETE')
+}
+
 export async function getFlight(id) {
     return sendRequest(`${BASE_URL}/api/flight/${id}`)
 }
 
-// export function deleteHoliday(id) {
-//     return sendRequest(`${BASE_URL}/api/holiday/delete/${id}`,'DELETE')
-// }
-
-// export async function imageUpload(formData){
-//     const response = await axios.post(
-//         `${BASE_URL}/api/upload/image`,
-//         formData
-//       )
-//       return response;
-// }
+export async function getHoliday(id) {
+    return sendRequest(`${BASE_URL}/api/holiday/${id}`)
+}
