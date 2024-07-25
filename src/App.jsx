b679085e9b651ffe7b9b52854cfbcfe814e6afe9
Marcus Lim <marcus.lwc7@gmail.com>
2024-07-25 21:08:01 +0800

    optimizing mobile 2

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
 
   return (
     <div className="flex flex-col md:flex-row">
-      <nav className="w-full md:w-64 min-h-screen bg-gray-800 text-white flex flex-col md:flex-col">
+      <nav className="w-full h-50 md:w-64 md:h-screen bg-gray-800 text-white flex flex-col md:flex-col">
         <div className="logo-container p-4 flex-shrink-0 hidden md:block">
           {!user ? (
             <Link to="/">
@@ -112,4 +112,5 @@ export default function App() {
       </main>
     </div>
   );
+  
 }  
